cedff2b85d0be80d0807eeff5df3c079ba0014f4
Paul <mateandmetal@gmail.com>
2022-06-16 18:30:25 -0300

    Added JS code

---
+++ javascript/01_comments.js
@@ -0,0 +1,10 @@
+
+// Single line comments
+let x = 5;
+//let y = 14;
+
+/*
+ * Multi line comments
+ * blah blah blah
+ * etc etc etc
+*/
---
+++ javascript/02_variables.js
@@ -0,0 +1,22 @@
+
+/*
+Variable names:
+- can contain letters, digits, underscores, and dollar signs
+- must begin with a letter, or can also begin with $ and _
+- names are case sensitive
+*/
+
+// Compatible with old browsers
+var x = 5;
+
+let y = 14;
+
+// Value will never change
+const PLAYER_MOVE_SPEED = 25.3
+
+// Strings can be written inside double or single quotes
+let name = 'Paul';
+let surname = "Ross";
+
+// Variables can be declared and initialized in one line
+let one = 'One', two = 'Two';
